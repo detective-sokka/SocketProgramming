9c66c5cfceeaca704a10065667dd103f5531047f
detectivesokka <dutt.sa@northeastern.edu>
2024-05-05 19:27:02 -0400

    Add basic validations for udp unicast

--- src/posix/udp_unicast.cpp
+++ src/posix/udp_unicast.cpp
@@ -1,10 +1,30 @@
 #include "include/posix/udp_unicast.h"
 
+#include <stdexcept>
+#include <arpa/inet.h>
+
 namespace posix
 {
 udp_unicast::udp_unicast(std::string udp_ip, unsigned short udp_port)
 {
     (void) udp_ip;
     (void) udp_port;
+    
+    if (udp_ip == "")
+    {
+        throw std::runtime_error{"Invalid UDP IP"};
+    }
+
+    struct sockaddr_in sa;
+
+    if (inet_pton(AF_INET, udp_ip.c_str(), &(sa.sin_addr)) != 1)
+    {
+        throw std::runtime_error{"Invalid UDP IP"};
+    }
+
+    if (udp_port < 1)
+    {
+        throw std::runtime_error{"Invalid UDP Port"};
+    }    
 }
 }
--- test/posix/udp_unicast_test.cc
+++ test/posix/udp_unicast_test.cc
@@ -4,9 +4,46 @@
 
 #include "include/posix/udp_unicast.h"
 
-TEST(udp_unicast_create_invalid_throw_exception, BasicAssertions)
+TEST(udp_unicast_create_invalid_ip_port_throws_exception, BasicAssertions)
 {
-    EXPECT_THROW(posix::udp_unicast("", 0), std::runtime_error);
+    EXPECT_THROW({
+        try
+        {
+            posix::udp_unicast("", 0);
+        }
+        catch(const std::runtime_error &e)
+        {
+            EXPECT_STREQ( "Invalid UDP IP", e.what());
+            throw;
+        }            
+    }, std::runtime_error);
 
-    std::cout << "test executed";
+    EXPECT_THROW({
+        try
+        {
+            posix::udp_unicast("127.0.0.1", 0);
+        }
+        catch(const std::runtime_error &e)
+        {
+            EXPECT_STREQ( "Invalid UDP Port", e.what());
+            throw;
+        }            
+    }, std::runtime_error);
+
+    EXPECT_THROW({
+        try
+        {
+            posix::udp_unicast("127.abc.0.1", 1);
+        }
+        catch(const std::runtime_error &e)
+        {
+            EXPECT_STREQ( "Invalid UDP IP", e.what());
+            throw;
+        }            
+    }, std::runtime_error);
+}
+
+TEST(udp_unicast_create_valid_ip_port_throws_nothing, BasicAssertions)
+{
+    EXPECT_NO_THROW(posix::udp_unicast("196.168.1.0", 22));
 }
